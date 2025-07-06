f71fa5a39bd23112e30f882bd2f66a92e829d976
accrazed <accrazed@proton.me>
2025-07-06 12:06:33 -0700

    refactor: rm string concats in favor of format! refactor: use random_range over modulo chore: comments feat: add F and i

--- src/parsers/basic.rs
+++ src/parsers/basic.rs
@@ -18,7 +18,6 @@ impl Aegyorruptor {
 
     pub fn parse(&mut self, input: String) -> String {
         let mut res = String::new();
-
         let mut cc = input.chars();
 
         while let Some(c) = cc.next() {
@@ -26,28 +25,31 @@ impl Aegyorruptor {
                 'b' => format!("૮{}ა", res),
                 'c' => self.corrupt_insert(res, cc.next().unwrap_or('0').to_digit(10).unwrap()),
                 'C' => self.corrupt_replace(res, cc.next().unwrap_or('0').to_digit(10).unwrap()),
-                'd' => res + "▿",
-                'e' => format!("◕{}&✿", res),
+                'd' => format!("{}▿", res),
+                'e' => format!("◕{}&", res),
                 'f' => format!("{}✿", res),
+                'F' => format!("˶{}˶", res),
                 'h' => format!("♡{}♡", res),
+                'i' => format!("{}※。*°☆", res),
                 'n' => format!("'.'.{}'.'.", res),
-                'o' => res + ".",
+                'o' => format!("{}.", res),
                 'p' => format!("({})", res),
-                's' => res + "ᴗ",
+                's' => format!("{}ᴗ", res),
                 't' => format!("₍{}₎", res),
                 'u' => format!("◡{}◡✿", res),
-                _ => res,
+                _ => format!("{}{}", res, c),
             }
         }
 
         res
     }
 
+    // corrupt_replace randomly replaces emoticon characters in-place with corrupted bytes
     fn corrupt_replace(&mut self, input: String, strength: u32) -> String {
         let mut res = String::new();
 
         for c in input.chars() {
-            match self.rand.random::<u32>() % strength {
+            match self.rand.random_range(..strength) {
                 0 => res.push(*CORRUPTION.choose(&mut self.rand).unwrap()),
                 _ => res.push(c),
             }
@@ -56,6 +58,7 @@ impl Aegyorruptor {
         res
     }
 
+    // corrupt_insert inserts a block of corrupted bytes into a random index of the emoticon
     fn corrupt_insert(&mut self, input: String, len: u32) -> String {
         let mut res = input.clone();
 
